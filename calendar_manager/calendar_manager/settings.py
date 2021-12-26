a8c96f73ddd77543f51397228bcd75cc3a6b8c78
dat.nd173011 <dat.nd173011@sis.hust.edu.vn>
2021-12-26 15:20:31 +0700

    Update settings

--- calendar_manager/calendar_manager/settings.py
+++ calendar_manager/calendar_manager/settings.py
@@ -87,7 +87,12 @@ DATABASES = {
         'PORT': '3306',
     }
 }
-
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
