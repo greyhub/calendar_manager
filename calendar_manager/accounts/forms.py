55bd42aa9523c5b07875c9a76b4ecdf118dd5e06
dat.nd173011 <dat.nd173011@sis.hust.edu.vn>
2021-12-26 14:34:41 +0700

    Update accounts

--- calendar_manager/accounts/forms.py
+++ calendar_manager/accounts/forms.py
@@ -1,47 +1,37 @@
 from django import forms
 from django.core.exceptions import ValidationError
 from django.contrib.auth.password_validation import validate_password
-from django.contrib.auth.forms import ReadOnlyPasswordHashField
+
 from accounts.models import User
 
 
 class SignInForm(forms.Form):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={
-        'class': 'form-control'
-    }))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={
-        'class': 'form-control'
-    }))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"class": "form-control"}))
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={"class": "form-control"})
+           )
 
 
 class SignUpForm(forms.ModelForm):
     password1 = forms.CharField(
-        label='Password',
-        widget=forms.PasswordInput(attrs={
-            'class': 'form-control'
-        }),
-        validators=[validate_password]
+        label="Password",
+        widget=forms.PasswordInput(attrs={"class": "form-control"}),
+        validators=[validate_password],
     )
     password2 = forms.CharField(
-        label='Confirm Password',
-        widget=forms.PasswordInput(attrs={
-            'class': 'form-control'
-        }),
-        validators=[validate_password]
+        label="Confirm Password",
+        widget=forms.PasswordInput(attrs={"class": "form-control"}),
+        validators=[validate_password],
     )
 
     class Meta:
         model = User
-        fields = ['email']
-        widgets = {
-            'email': forms.EmailInput(attrs={
-                'class': 'form-control'
-            })
-        }
+        fields = ["email"]
+        widgets = {"email": forms.EmailInput(attrs={"class": "form-control"})}
 
     def clean_password2(self):
-        password1 = self.cleaned_data.get('password1')
-        password2 = self.cleaned_data.get('password2')
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
         if password1 and password2 and password1 != password2:
             raise ValidationError("Password didn't match!")
         return password2
@@ -52,52 +42,3 @@ class SignUpForm(forms.ModelForm):
         if commit:
             user.save()
         return user
-
-class UserAdminCreationForm(forms.ModelForm):
-    """
-    A form for creating new users. Includes all the required
-    fields, plus a repeated password.
-    """
-    password = forms.CharField(widget=forms.PasswordInput)
-    password_2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
-
-    class Meta:
-        model = User
-        fields = ['email']
-
-    def clean(self):
-        '''
-        Verify both passwords match.
-        '''
-        cleaned_data = super().clean()
-        password = cleaned_data.get("password")
-        password_2 = cleaned_data.get("password_2")
-        if password is not None and password != password_2:
-            self.add_error("password_2", "Your passwords must match")
-        return cleaned_data
-
-    def save(self, commit=True):
-        # Save the provided password in hashed format
-        user = super().save(commit=False)
-        user.set_password(self.cleaned_data["password"])
-        if commit:
-            user.save()
-        return user
-
-
-class UserAdminChangeForm(forms.ModelForm):
-    """A form for updating users. Includes all the fields on
-    the user, but replaces the password field with admin's
-    password hash display field.
-    """
-    password = ReadOnlyPasswordHashField()
-
-    class Meta:
-        model = User
-        fields = ['email', 'password', 'is_active', 'admin']
-
-    def clean_password(self):
-        # Regardless of what the user provides, return the initial value.
-        # This is done here, rather than on the field, because the
-        # field does not have access to the initial value
-        return self.initial["password"]
