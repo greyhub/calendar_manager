b1f94c151700f072c41ac332105fc420686cd955
greyhub <luongnv.grey@gmail.com>
2022-01-16 20:32:17 +0700

    Update pipeline

--- optimize_calendar/pipeline.py
+++ optimize_calendar/pipeline.py
@@ -1,6 +1,9 @@
 from preprocess import preprocess
 from solver.LinearSolver import suggest
 
-def main():
-    preprocess(input='data/Demo_data.json', dir='data/')
-    suggest(year=2022, week=2, dir='data/')
+def main(input, data_dir, year, week):
+    preprocess(input=input, dir=data_dir)
+    suggest(year=year, week=week, dir=data_dir)
+
+if __name__ == '__main__':
+    main(input='data/Demo_data.json', data_dir='data/', year=2022, week=2)
